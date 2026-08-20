1b956003bb4b9eb1fad43b549bba3be21e25a3ae
UbuntuVM <ubuntu-vm@android.local>
2026-08-20 09:01:57 -0300

    Refactor package name to com.thiago.ubuntuvm to eliminate APK installation conflict

--- UbuntuVMApp/app/src/main/java/com/thiago/ubuntuvm/MainActivity.kt
+++ UbuntuVMApp/app/src/main/java/com/thiago/ubuntuvm/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ubuntu.vmmanager
+package com.thiago.ubuntuvm
 
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
@@ -14,6 +14,7 @@ import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -129,7 +130,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupOnBackPressed() {
-        onBackPressedDispatcher.addCallback(this, object : androidx.activity.OnBackPressedCallback(true) {
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 if (webView.canGoBack()) {
                     webView.goBack()
